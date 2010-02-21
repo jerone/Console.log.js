32d22cb88a10413fe9e870b842afbd165053c30b
jerone <jeronevw@hotmail.com>
2010-02-22 00:01:24 +0100

    * ADD: added external function support;
    * MOD: cleaned up code;

--- console.log.js
+++ console.log.js
@@ -3,7 +3,7 @@ Console.log.js - Cross-Browser solution for console logging in JavaScript (coden
 
 @author Jeroen van Warmerdam (aka jerone or jeronevw) (http://www.jeroenvanwarmerdam.nl)
 @date 14-02-2010 16:30
-@version 0.1 Beta
+@version 0.1 RC1
 
 Copyright 2010, Jeroen van Warmerdam
 
@@ -95,9 +95,10 @@ XB (CrossBrowser):
 |							empty RegExp returns "//" instead of "/(?:)/";
 
 TODO:
-* ADD: anti alert flood;
 * FIX: multiple lines alignement;
+* ADD: anti alert flood;
 * ADD: add latest console.profile() and console.profileEnd();
+* ADD: line numbers;
 
 */
 
@@ -110,7 +111,7 @@ TODO:
 		_loggingFn = ["log", "debug", "info", "warn", "error"],
 		_specialFn = ["assert", "clear", "count", "dir", "dirxml", "group", "groupCollapsed", "groupEnd", "profile", "profileEnd", "time", "timeEnd", "trace"];
 
-	_w_c.ConsoleLogJS = "0.1b";  // version number;
+	_w_c.ConsoleLogJS = "0.1rc1";  // version number;
 
 	_w_c_s = _w_c.settings = {
 		override: _w_c_s.override !== undefined ? _w_c_s.override : true, 		// override the console function;
@@ -120,48 +121,45 @@ TODO:
 		limit: _w_c_s.limit !== undefined ? _w_c_s.limit : 3					// limit how deed to give source;
 	};
 
-	if(_w_c_s.override !== false && !_w_c.firebug) {  // we know Firebug has these functions;
+	if(!!_w_c_s.override && (!_w_c.firebug || _obj.call(_w_c_s.override) === "[object Function]" || _obj.call(_w_c_s.override) === "[object Object]")) {  // we know Firebug has these functions;
 
 		var IE = (/msie/i.test(_uA) && !/opera/i.test(_uA)),
 			WebKit = / AppleWebKit\//.test(_uA),
 			Pattern = /%([sdifo])/gi,  // substitution patterns;
+			Override = function(type) {
+				return (!!_w_c_s.override && (
+							(_obj.call(_w_c_s.override) === "[object Function]" && _w_c_s.override) ||
+							(_obj.call(_w_c_s.override) === "[object Object]" && (type && _w_c_s.override[type] || _w_c_s.override["log"]))
+						) || false);
+			},
 			Console = function(type) {
-				return (_w_c_s.ie_dt && IE && (type && _w_c[type] || _w_c["log"]))
+				return (Override(type))
+					|| (_w_c_s.ie_dt && IE && (type && _w_c[type] || _w_c["log"]))
 					|| (_w_c_s.opera && _w.opera && opera.postError)
 					|| (_w_c_s.alert && alert)
 					|| function() { return undefined; };
 			},
 			Source = function(arg, limit) {
 				var result = "";
-
 				if(arg === null) {  // null;
 					result = "null";
-				}
-				else if(typeof arg === "undefined" || typeof arg === undefined) {  // undefined;
+				} else if(typeof arg === "undefined" || typeof arg === undefined) {  // undefined;
 					result = "undefined";
-				}
-				else if(arg === true || arg === false) {  // Boolean;
+				} else if(arg === true || arg === false) {  // Boolean;
 					result = !!arg ? "true" : "false";
-				}
-				else if(arg === "") {  // empty String (before normal String);
+				} else if(arg === "") {  // empty String (before normal String);
 					result = "";
-				}
-				else if(_obj.call(arg) === "[object String]") {  // String;
+				} else if(_obj.call(arg) === "[object String]") {  // String;
 					result = '"' + arg + '"';
-				}
-				else if(_obj.call(arg) === "[object Date]") {  // Date;
+				} else if(_obj.call(arg) === "[object Date]") {  // Date;
 					result = arg.toString();
-				}
-				else if(_obj.call(arg) === "[object Number]") {  // Number;
+				} else if(_obj.call(arg) === "[object Number]") {  // Number;
 					result = arg.toString();
-				}
-				else if(_obj.call(arg) === "[object Navigator]" || (arg.constructor && arg.constructor.toString() === "[object Navigator]")) {  // Navigator Element (before Element, Array & Object);
+				} else if(_obj.call(arg) === "[object Navigator]" || (arg.constructor && arg.constructor.toString() === "[object Navigator]")) {  // Navigator Element (before Element, Array & Object);
 					result = "Navigator";
-				}
-				else if(_obj.call(arg) === "[object Window]" || (arg.constructor && arg.constructor.toString() === "[object Window]")) {  // Window Element (before Element, Array & Object);
+				} else if(_obj.call(arg) === "[object Window]" || (arg.constructor && arg.constructor.toString() === "[object Window]")) {  // Window Element (before Element, Array & Object);
 					result = "Window";
-				}
-				else if(arg.nodeType) {  // Element (before Array & Object);
+				} else if(arg.nodeType) {  // Element (before Array & Object);
 					if(arg.nodeType === 3 || arg.nodeName === "#text") {  // text node;
 						result = arg.textContent || arg.nodeValue;
 					} else if(arg.nodeType === 8 || arg.nodeName === "#comment") {  // comment node;
@@ -181,25 +179,21 @@ TODO:
 					} else {
 						result = arg.toString();
 					}
-				}
-				else if(_obj.call(arg) === "[object Array]") {  // Array;
+				} else if(_obj.call(arg) === "[object Array]") {  // Array;
 					result = "[";
 					var arr_list = [], i = 0, l = arg.length;
 					for(; i < l; i++) {
 						arr_list[i] = Source(arg[i], limit);
 					}
 					result += arr_list.join(", ") + "]";
-				}
-				else if(_obj.call(arg) === "[object RegExp]" || arg instanceof RegExp) {  // RegExp (before Function);
+				} else if(_obj.call(arg) === "[object RegExp]" || arg instanceof RegExp) {  // RegExp (before Function);
 					result = (arg.valueOf() ||
 							  arg.toString() ||
 							  "/" + arg.source + "/" + (arg.global ? "g" : "") + (arg.ignoreCase ? "i" : "") + (arg.multiline ? "m" : "") + (arg.sticky ? "y" : "")
 							 ).toString().replace(/^\/\/$/, "\/\(?:)\/").replace(/^(\/.*\/)(ig)(m?)$/g, "$1gi$3");
-				}
-				else if(_obj.call(arg) === "[object Function]") {  // Function;
+				} else if(_obj.call(arg) === "[object Function]") {  // Function;
 					result = arg.toString();
-				}
-				else if(typeof arg === "object") {  // Object (last);
+				} else if(typeof arg === "object") {  // Object (last);
 					if(!limit) { return "{ more... }"; }
 					result = "{ ";
 					var arr_obj = [];
@@ -207,11 +201,9 @@ TODO:
 						arr_obj.push("'" + key + "': " + Source(arg[key], limit - 1));
 					}
 					result += arr_obj.join(", ") + " }";
-				}
-				else {  // ...
+				} else {  // ...
 					result = arg.toString();
 				}
-
 				return result;
 			},
 			Special = function(fx) {
@@ -222,12 +214,79 @@ TODO:
 								_w_c.error.call({ internal: "ASSERT FAIL: " }, message);
 							}
 						};
+					case "clear":
+						return function() {
+							var ln = "\n \n";
+							if(!WebKit) {
+								_w_c.log.call({ internal: ln }, "");
+								_w_c.log.call({ internal: ln }, "");
+								_w_c.log.call({ internal: ln }, "");
+							} else {
+								_w_c.log(ln);
+								_w_c.log(ln);
+								_w_c.log(ln);
+							}
+							ln = null;
+						};
 					case "count":
 						return function(title) {
 							title = title || "";
 							var acc = arguments.callee._counters = arguments.callee._counters || {};
 							_w_c.log.call({ internal: (title !== "" ? title + " " : "") + (acc[title] = ++acc[title] || 1) }, "");
 							title = acc = null;
+						};
+					case "dir":
+						return function(arg) {
+							var result = "";
+							if(_obj.call(arg) === "[object String]") {  // String;
+								arg = arg.split("");
+							}
+							try {
+								for(var item in arg) {
+									result += item.toString() + "\t\t";
+									try {
+										result += Source(arg[item], _w_c_s.limit) + "\n";
+									} catch(e) {  // probably Packages, sun, java, netscape or external;
+										result += item.toString() + "\n";
+									}
+								}
+							} catch(e) {  // probably Packages, sun, java, netscape or external;
+								result += arg.toString() + "\n";
+							}
+							_w_c.log.call({ internal: result }, "");
+							arg = result = null;
+						};
+					case "dirxml":
+						return function(node) {
+							if(node && node.nodeType) {  // Element;
+								var SourceXML = function(node, limit) {
+									var result,
+										begin = result = Source(node, _w_c_s.limit),
+										i = 0, l = node.childNodes.length,
+										closingTag = /\<([a-z]*)\b/i,
+										noTextNode = false;
+									if(l > 0) {
+										for(; i < l; i++) {
+											var newNode = node.childNodes[i];
+											if((noTextNode = newNode.nodeType !== 3 && newNode.nodeName !== "#text")) {
+												result += "\n\t";
+											}
+											if(limit > 0 && newNode.childNodes.length > 0) {
+												result += SourceXML(newNode, limit - 1);
+											} else {
+												result += Source(newNode, _w_c_s.limit);
+											}
+										}
+										if(noTextNode) {
+											result += "\n\t";
+										}
+										result += "</" + begin.match(closingTag)[1] + ">";  // closing tag;
+									}
+									node = limit = begin = i = l = closingTag = noTextNode = null;
+									return result;
+								}
+								_w_c.log.call({ internal: SourceXML(node, _w_c_s.limit) }, "");
+							}
 						};
 					case "group":
 					case "groupCollapsed":
@@ -266,69 +325,6 @@ TODO:
 								_w_c.log((e.stack || e.stacktrace).split("\n").slice(2).join("\n"));
 							}
 						};
-					case "clear":
-						return function() {
-							var ln = "\n \n";
-							if(!WebKit) {
-								_w_c.log.call({ internal: ln }, "");
-								_w_c.log.call({ internal: ln }, "");
-								_w_c.log.call({ internal: ln }, "");
-							} else {
-								_w_c.log(ln);
-								_w_c.log(ln);
-								_w_c.log(ln);
-							}
-							ln = null;
-						};
-					case "dir":
-						return function(arg) {
-							var result = "";
-							if(_obj.call(arg) === "[object String]") {  // String;
-								arg = arg.split("");
-							}
-							try {
-								for(var item in arg) {
-									result += item.toString() + "\t\t";
-									try {
-										result += Source(arg[item], _w_c_s.limit) + "\n";
-									} catch(e) {  // probably Packages, sun, java, netscape or external;
-										result += item.toString() + "\n";
-									}
-								}
-							} catch(e) {  // probably Packages, sun, java, netscape or external;
-								result += arg.toString() + "\n";
-							}
-							_w_c.log.call({ internal: result }, "");
-							arg = result = null;
-						};
-					case "dirxml":
-						return function(node) {
-							if(node && node.nodeType) {  // Element;
-								var SourceXML = function(node, limit) {
-									var result, begin = result = Source(node, _w_c_s.limit), i = 0, l = node.childNodes.length, closingTag = /\<([a-z]*)\b/i, noTextNode = false;
-									if(l > 0) {
-										for(; i < l; i++) {
-											var newNode = node.childNodes[i];
-											if((noTextNode = newNode.nodeType !== 3 && newNode.nodeName !== "#text")) {
-												result += "\n\t";
-											}
-											if(limit > 0 && newNode.childNodes.length > 0) {
-												result += SourceXML(newNode, limit - 1);
-											} else {
-												result += Source(newNode, _w_c_s.limit);
-											}
-										}
-										if(noTextNode) {
-											result += "\n\t";
-										}
-										result += "</" + begin.match(closingTag)[1] + ">";  // closing tag;
-									}
-									node = limit = begin = i = l = closingTag = noTextNode = null;
-									return result;
-								}
-								_w_c.log.call({ internal: SourceXML(node, _w_c_s.limit) }, "");
-							}
-						};
 					case "profile":
 					case "profileEnd":
 					default:
@@ -337,16 +333,18 @@ TODO:
 			};
 
 		for(var i in _loggingFn) {
-			var _fn = _loggingFn[i];
-			if(!_w_c[_fn] || (IE && _w.console)) {  // console[_loggingFn] doesn't exist and we override IE Developer Toolbar console to gain more arguments information;
-				_w_c[_fn] = (function(type) {
-					var _Console = Console(type);
+			var _type = _loggingFn[i];
+			if(!_w_c[_type] || (IE && _w.console) || !!Override(_type)) {  // console[_loggingFn] doesn't exist and we override IE Developer Toolbar console to gain more arguments information;
+				_w_c[_type] = (function(type) {
 					return function() {
-						var prefix = IE ? "" :
-										type == "info" ? "(i): " :
-											type == "warn" ? "/!\\: " :
-												type == "error" ? "(X): " :
-													"",
+						var prefix = IE ? "" : (function() {
+								switch(type) {
+									case "info": return "(i): ";
+									case "warn": return "/!\\: ";
+									case "error": return "(X): ";
+									default: return "";
+								}
+							})(),
 							_Source = arguments[0],
 							i = 0, l = arguments.length,
 							limit = _w_c_s.limit,
@@ -374,17 +372,17 @@ TODO:
 								}
 							}
 						}
-						_Console(align + prefix + (this.internal || "") + _Source);
+						(Console(type))(align + prefix + (this.internal || "") + _Source);
 						prefix = _Source = i = l = limit = null;
 						return arguments;
 					};
-				})(_fn);
+				})(_type);
 			}
 		}
 		for(var i in _specialFn) {
-			var _fn = _specialFn[i];
-			if(!_w_c[_fn]) {  // no console[_specialFn];
-				_w_c[_fn] = Special(_fn);
+			var _type = _specialFn[i];
+			if(!_w_c[_type]) {  // no console[_specialFn];
+				_w_c[_type] = Special(_type);
 			}
 		}
 
